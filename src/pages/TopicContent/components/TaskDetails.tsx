8277a2c5453f645c1aa6a52a9aa6d6576e34bab9
akmatoff <akmatoff.exe@gmail.com>
2024-05-07 18:28:26 +0600

    Fix task details layout

--- src/pages/TopicContent/components/TaskDetails.tsx
+++ src/pages/TopicContent/components/TaskDetails.tsx
@@ -33,39 +33,41 @@ function TaskDetails({ task }: Props) {
   if (!task) return null;
 
   return (
-    <div className="flex flex-col gap-4 items-center justify-center py-10 lg:px-32">
-      {!!task.correctAnswerExplanation && task.isCompleted && (
-        <div className="flex gap-2 p-4 text-secondary w-full bg-bgSecondary rounded-xl border-1 border-secondary">
-          {<Icons.EXCLAMATION className="text-xl" />}{" "}
-          {task.correctAnswerExplanation}
+    <div className="flex flex-col gap-4 items-center justify-center py-10">
+      <div className="flex flex-col gap-4">
+        {!!task.correctAnswerExplanation && task.isCompleted && (
+          <div className="flex gap-2 p-4 text-secondary w-full bg-bgSecondary rounded-xl border-1 border-secondary">
+            {<Icons.EXCLAMATION className="text-xl" />}{" "}
+            {task.correctAnswerExplanation}
+          </div>
+        )}
+        <div className="flex flex-col items-center p-6 bg-bgSecondary rounded-xl w-full">
+          <div dangerouslySetInnerHTML={{ __html: task?.text || "" }} />
         </div>
-      )}
-      <div className="flex flex-col items-center p-6 bg-bgSecondary rounded-xl w-full">
-        <div dangerouslySetInnerHTML={{ __html: task?.text || "" }} />
-      </div>
-      <div className="grid w-full gap-4 grid-cols-2">
-        {task.answers.map((answer) => (
+        <div className="grid w-full gap-4 grid-cols-2">
+          {task.answers.map((answer) => (
+            <Button
+              key={answer.id}
+              onPress={() => setSelectedAnswer(answer)}
+              className={
+                selectedAnswer === answer ? "bg-secondary text-background" : ""
+              }
+            >
+              {answer.text}
+            </Button>
+          ))}
+        </div>
+        {!task.isCompleted && (
           <Button
-            key={answer.id}
-            onPress={() => setSelectedAnswer(answer)}
-            className={
-              selectedAnswer === answer ? "bg-secondary text-background" : ""
-            }
+            color="primary"
+            isDisabled={!selectedAnswer}
+            isLoading={isPending}
+            onPress={handleCheckAnswer}
           >
-            {answer.text}
+            Подтвердить
           </Button>
-        ))}
+        )}
       </div>
-      {!task.isCompleted && (
-        <Button
-          color="primary"
-          isDisabled={!selectedAnswer}
-          isLoading={isPending}
-          onPress={handleCheckAnswer}
-        >
-          Подтвердить
-        </Button>
-      )}
     </div>
   );
 }
