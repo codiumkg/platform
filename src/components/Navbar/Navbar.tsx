bc23f3d8b6b215470802a34898330a8c4bfc8f7b
akmatoff <akmatoff.exe@gmail.com>
2024-05-21 00:55:14 +0600

    Update navbar, include link to home page

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import CodiumLogo from "@/assets/codium_logo_solo.png";
+import CodiumLogoFull from "@/assets/codium_logo.png";
 import { ROUTES } from "@/constants/routes";
 import useAuth from "@/hooks/useAuth";
 import { useUserData } from "@/queries/userdata";
@@ -7,6 +8,7 @@ import {
   DropdownItem,
   DropdownMenu,
   DropdownTrigger,
+  Image,
 } from "@nextui-org/react";
 import { useNavigate } from "react-router";
 
@@ -18,33 +20,44 @@ function Navbar() {
   const { data: userData } = useUserData({ enabled: checkIsLoggedIn() });
 
   return (
-    <div className="fixed flex justify-end w-screen p-3 select-none backdrop-blur">
-      {checkIsLoggedIn() && (
-        <Dropdown aria-label="Dropdown">
-          <DropdownTrigger aria-label="trigger">
-            <div className="h-13 md:h-16 bg-bgSecondary border border-highlight rounded-full p-4 cursor-pointer">
-              <div className="flex flex-row items-center">
-                <img
-                  src={CodiumLogo}
-                  className={`w-5 h-5 md:w-8 md:h-8 ${userData ? "mr-3" : ""}`}
-                />
-                {userData?.username && (
-                  <h1 className="text-sm">{userData?.username}</h1>
-                )}
+    <header className="h-20 w-screen fixed">
+      <div className="flex h-full w-full items-center justify-between py-4 px-8 select-none backdrop-blur-md">
+        <Image
+          src={CodiumLogoFull}
+          width={170}
+          className="cursor-pointer"
+          onClick={() => navigate(ROUTES.HOME)}
+        />
+
+        {checkIsLoggedIn() && (
+          <Dropdown aria-label="Dropdown">
+            <DropdownTrigger aria-label="trigger">
+              <div className="cursor-pointer">
+                <div className="flex flex-row items-center">
+                  <img
+                    src={CodiumLogo}
+                    className={`w-5 h-5 md:w-8 md:h-8 ${
+                      userData ? "mr-3" : ""
+                    }`}
+                  />
+                  {userData?.username && (
+                    <h1 className="text-sm">{userData?.username}</h1>
+                  )}
+                </div>
               </div>
-            </div>
-          </DropdownTrigger>
-          <DropdownMenu aria-label="menu">
-            <DropdownItem onPress={() => navigate(ROUTES.PROFILE)}>
-              Профиль
-            </DropdownItem>
-            <DropdownItem onPress={logout} aria-label="Выйти">
-              Выйти
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-      )}
-    </div>
+            </DropdownTrigger>
+            <DropdownMenu aria-label="menu">
+              <DropdownItem onPress={() => navigate(ROUTES.PROFILE)}>
+                Профиль
+              </DropdownItem>
+              <DropdownItem onPress={logout} aria-label="Выйти">
+                Выйти
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        )}
+      </div>
+    </header>
   );
 }
 
